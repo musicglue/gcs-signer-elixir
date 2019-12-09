ecaa5d8618039e4404bac64fa60ddec9895b16ae
Piotr Szmielew <p.szmielew@ava.waw.pl>
2019-12-09 15:36:54 +0100

    Add option for loading keys from env on GCP

--- lib/gcs_signer/client.ex
+++ lib/gcs_signer/client.ex
@@ -6,22 +6,40 @@ defmodule GcsSigner.Client do
   defstruct private_key: nil, client_email: nil
 
   @doc """
-  Initialize GcsSigner.Client from given keyfile.
+  Initialize GcsSigner.Clien
 
   ## Examples
 
       iex> service_account = service_account_json_str |> Poison.decode!
-      iex> GcsSigner.Client.from_keyfile(service_account)
+      iex> GcsSigner.Client.init(service_account)
+      %GcsSigner.Client{...}
+
+
+      # Load from env variables
+      iex> GcsSigner.Client.init()
       %GcsSigner.Client{...}
 
   """
-  def from_keyfile(%{
-    "private_key" => private_key,
-    "client_email" => client_email,
-  }) do
+  def init(), do: look_for_environment_variables() |> Poison.decode!() |> from_keyfile()
+  def init(keyfile_map) when is_map(keyfile_map), do: from_keyfile(keyfile_map)
+
+  defp from_keyfile(%{
+         "private_key" => private_key,
+         "client_email" => client_email
+       }) do
     %GcsSigner.Client{
       private_key: private_key,
       client_email: client_email
     }
+  end
+
+  defp look_for_environment_variables() do
+    case System.fetch_env("GOOGLE_CLOUD_KEYFILE") do
+      {:ok, keyfile} ->
+        keyfile |> File.read!()
+
+      _ ->
+        System.get_env("GOOGLE_CLOUD_KEYFILE_JSON")
+    end
   end
 end
